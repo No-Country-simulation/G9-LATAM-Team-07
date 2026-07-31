b8ba33c9da308a3f27117e439b1eaea14345118e
Maximiliano Pizarro <pisa88@live.cl>
2026-07-31 10:59:56 -0400

    registrar Bean de RestTemplate en TechmindApplication
    Se agrega la configuración del Bean RestTemplate para permitir la inyección de dependencias en el cliente HTTP que consume el microservicio de Python

--- api-java/src/main/java/com/techmind/api/TechmindApplication.java
+++ api-java/src/main/java/com/techmind/api/TechmindApplication.java
@@ -2,10 +2,18 @@ package com.techmind.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TechmindApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TechmindApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
