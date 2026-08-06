7b0870f44bba4f23b6a1e843ebb5410869d71faa
Maximiliano Pizarro <pisa88@live.cl>
2026-08-06 14:27:37 -0400

    Add constructor for ServicioInferenciaException with cause

--- api-java/src/main/java/com/techmind/api/exceptions/ServicioInferenciaException.java
+++ api-java/src/main/java/com/techmind/api/exceptions/ServicioInferenciaException.java
@@ -1,9 +1,12 @@
 package com.techmind.api.exceptions;
 
-
 public class ServicioInferenciaException extends RuntimeException {
 
     public ServicioInferenciaException(String mensaje) {
         super(mensaje);
+    }
+
+    public ServicioInferenciaException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
     }
 }
